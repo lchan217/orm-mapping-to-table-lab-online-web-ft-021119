4bd783cf79b07fb7d0b4666827faddcc2cce3662
Lizzie Chan <lchan217@gmail.com>
2019-03-26 17:06:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
   end 
   def save 
     sql = <<-SQL
-      INSERT INTO students (name, age) VALUES (?,?), 
+      "INSERT INTO students (name, age) VALUES (?,?)", name, age 
     SQL
   end 
 end
